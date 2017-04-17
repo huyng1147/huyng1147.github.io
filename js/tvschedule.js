a5f7e79e1eddb94fefd286ea744b031e2ebd0e2c
huyng1147 <huy.ng1147@gmail.com>
2017-04-16 19:55:49 -0500

    Layout mansard

--- js/tvschedule.js
+++ js/tvschedule.js
@@ -5,16 +5,10 @@
 
 const TVMAZE_URL = "https://api.tvmaze.com/schedule?country=US&date=";
 
-$(function() {
-    console.log("ready");
-    getScheduleCurrentDate();
-});
+var currentday;
+var $gridshow = $("#tvschedule");
 
-
-
-
-function getScheduleCurrentDate() {
-    var date = new Date();
+function getDateStringFromDate(date) {
     var dd = date.getDate();
     var mm = date.getMonth() + 1;
     var yyyy = date.getFullYear();
@@ -24,9 +18,65 @@ function getScheduleCurrentDate() {
     if (dd < 10) {
         dd = '0' + dd;
     }
+    return yyyy + '-' + mm + '-' + dd;
+}
+
+function getHeadingDateStringFromDate(date) {
+    var dd = date.getDate();
+    var mm = date.getMonth() + 1;
+    var yyyy = date.getFullYear();
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+    return mm + "/" + dd + "/" + yyyy;
+}
+
+$(function() {
+    console.log("ready tv");
+    var date = new Date();
+    currentday = date;
+    
+    $("#todayheading").text(getHeadingDateStringFromDate(currentday));
+    getScheduleCurrentDate(getDateStringFromDate(currentday));
+    // loadGrid();
+
+    $("#preBtn").click(function(event) {
+        getSchedulePreDate();
+    });
+
+    $("#nextBtn").click(function(event) {
+        getScheduleNextDate();
+    });
+});
+
+
+
+function getScheduleNextDate() {
+    currentday.setDate(currentday.getDate() + 1);
+    $("#todayheading").text(getHeadingDateStringFromDate(currentday));
+    getScheduleCurrentDate(getDateStringFromDate(currentday));
+
+}
+
+function getSchedulePreDate() {
+    currentday.setDate(currentday.getDate() - 1);
+    $("#todayheading").text(getHeadingDateStringFromDate(currentday));
+    getScheduleCurrentDate(getDateStringFromDate(currentday));
+}
+
+
+function getScheduleCurrentDate(today) {
+    $gridshow.masonry({
+        transitionDuration: '0.4s',
+        initLayout: false
+    });
+
     $.ajax({
             type: "GET",
-            url: TVMAZE_URL + yyyy + '-' + mm + '-' + dd,
+            url: TVMAZE_URL + today
         })
         .done(
             function(result) {
@@ -45,31 +95,32 @@ function getScheduleCurrentDate() {
                             if (summary == null) {
                                 summary = "N/A"
                             }
-                            $("#tvschedule").append(
-                                '<div class="col-md-4">' +
-                                '<div class="panel panel-primary">' +
-                                '<div class="panel-heading">' +
-                                '<h3 class="panel-title">' + show.show.name + '</h3>' +
-                                '</div>' +
-                                '<div class="panel-body">' +
-                                '<div class="col-md-8">' +
-                                '<div>Title: ' + show.name + '</div>' +
-                                '<div>Season: ' + show.season + '</div>' +
-                                '<div>Episode: ' + show.number + '</div>' +
-                                '<div>Time: ' + show.show.schedule.time + '</div>' +
-                                '<div>Channel: ' + show.show.network.name + '</div>' +
-                                '<div>Rating: ' + rating + '</div>' +
-                                '<a type="button" class="btn btn-link" style="padding-left: 0px;" href="' + show.url + '" target="_blank">Link</a>' +
-                                '</div>' +
-                                '<div class="col-md-4">' +
-                                '<img style=" width: auto; height: 150px;" src="' + show.show.image.original + '"></img>' +
-                                '</div>' + 
-                                // '<div class="container-fluid">' + summary + '</div>' +
-                                '</div>' +
-                                '</div>' +
-                                '</div>')
+                            var $items = $('<div class="col-sm-4">' +
+                                    '<div class="panel panel-primary" style="margin-bottom: 10px;">' +
+                                        '<div class="panel-heading">' +
+                                            '<h3 class="panel-title">' + show.show.name + '</h3>' +
+                                        '</div>' +
+                                        '<div class="panel-body">' +
+                                            '<div class="col-lg-8">' +
+                                                '<div>Title: ' + show.name + '</div>' +
+                                                '<div>Season: ' + show.season + '</div>' +
+                                                '<div>Episode: ' + show.number + '</div>' +
+                                                '<div>Time: ' + show.show.schedule.time + '</div>' +
+                                                '<div>Channel: ' + show.show.network.name + '</div>' +
+                                                '<div>Rating: ' + rating + '</div>' +
+                                                '<a type="button" class="btn btn-link" style="padding-left: 0px;" href="' + show.url + '" target="_blank">Link</a>' +
+                                                '<div>Summary: ' + summary + '</div>' +
+                                            '</div>' +
+                                            '<div class="col-lg-4">' +
+                                                '<img class="img-responsive" src="' + show.show.image.original + '"></img>' +
+                                            '</div>' + 
+                                        '</div>' +
+                                    '</div>' +
+                                '</div>');
+                            $gridshow.append($items).masonry('prepended', $items);
                         }
                     }
+                    $gridshow.masonry();
                 }
             })
         .fail(function() {
